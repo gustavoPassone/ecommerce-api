c540398b38d5d3e56ae8fa09e99b19fb3e51e08a
gustavoPassone <gmpassone@gmail.com>
2025-11-25 09:40:32 -0300

    Criando servidor com Express

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bem vindo");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor ativo na porta 3000");
+});
