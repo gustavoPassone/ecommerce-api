1bf221b2e2474c1af7a2cf4381fdf7339a464520
gustavoPassone <gmpassone@gmail.com>
2025-11-26 10:54:43 -0300

    Metodos PUT e DELETE

--- src/index.ts
+++ src/index.ts
@@ -7,26 +7,51 @@ app.get("/", (req, res) => {
   res.send("Bem vindo");
 });
 
-let usuarios = [
-  {
-    nome: "Gustavo MP",
-    idade: 19,
-  },
-  {
-    nome: "João Silva",
-    idade: 44,
-  },
-];
+type User = {
+  id: number;
+  nome: string;
+  email: string;
+};
+
+let id = 0;
+let usuarios: User[] = [];
 
 app.get("/users", (req: Request, res: Response) => {
   res.send(usuarios);
 });
 
+app.get("/users/:id", (req: Request, res: Response) => {
+  let userId = Number(req.params.id);
+  let user = usuarios.find((user) => user.id === userId);
+  res.send(user);
+});
+
 app.post("/users", (req: Request, res: Response) => {
   let user = req.body;
+  user.id = ++id;
   usuarios.push(user);
   res.send({
     message: "Usuario criado com sucesso",
+  });
+});
+
+app.put("/users/:id", (req: Request, res: Response) => {
+  let userId = Number(req.params.id);
+  let user = req.body;
+  let indexOf = usuarios.findIndex((_user: User) => _user.id === userId);
+  usuarios[indexOf].nome = user.nome;
+  usuarios[indexOf].email = user.email;
+  res.send({
+    message: "Usuario alterado com sucesso",
+  });
+});
+
+app.delete("/users/:id", (req: Request, res: Response) => {
+  let userId = Number(req.params.id);
+  let indexOf = usuarios.findIndex((user: User) => user.id === userId);
+  usuarios.splice(indexOf, 1);
+  res.send({
+    message: "Usuario excluido com sucesso",
   });
 });
 
