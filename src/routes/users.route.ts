ca5aa0c434a7c7aae49002d88ae1bede6cda10c1
gustavoPassone <gmpassone@gmail.com>
2025-11-27 10:57:46 -0300

    Refatorar rotas de usuário para utilizar métodos do UsersController

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,51 +1,10 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import { UsersController } from "../controllers/users.controller";
 
 export const userRouters = express.Router();
 
-type User = {
-  id: number;
-  nome: string;
-  email: string;
-};
-
-let id = 0;
-let usuarios: User[] = [];
-
-userRouters.get("/users", (req: Request, res: Response) => {
-  res.send(usuarios);
-});
-
-userRouters.get("/users/:id", (req: Request, res: Response) => {
-  let userId = Number(req.params.id);
-  let user = usuarios.find((user) => user.id === userId);
-  res.send(user);
-});
-
-userRouters.post("/users", (req: Request, res: Response) => {
-  let user = req.body;
-  user.id = ++id;
-  usuarios.push(user);
-  res.send({
-    message: "Usuario criado com sucesso",
-  });
-});
-
-userRouters.put("/users/:id", (req: Request, res: Response) => {
-  let userId = Number(req.params.id);
-  let user = req.body;
-  let indexOf = usuarios.findIndex((_user: User) => _user.id === userId);
-  usuarios[indexOf].nome = user.nome;
-  usuarios[indexOf].email = user.email;
-  res.send({
-    message: "Usuario alterado com sucesso",
-  });
-});
-
-userRouters.delete("/users/:id", (req: Request, res: Response) => {
-  let userId = Number(req.params.id);
-  let indexOf = usuarios.findIndex((user: User) => user.id === userId);
-  usuarios.splice(indexOf, 1);
-  res.send({
-    message: "Usuario excluido com sucesso",
-  });
-});
+userRouters.get("/users", UsersController.getAll);
+userRouters.get("/users/:id", UsersController.getById);
+userRouters.post("/users", UsersController.save);
+userRouters.put("/users/:id", UsersController.update);
+userRouters.delete("/users/:id", UsersController.delete);
