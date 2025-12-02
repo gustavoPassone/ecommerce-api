acd2f82c9e6668f3e8830f8de4692769ed18e404
gustavoPassone <gmpassone@gmail.com>
2025-12-02 10:49:10 -0300

    feat: obtendo os dados da Collection - getAll

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -9,8 +9,15 @@ type User = {
 let usuarios: User[] = [];
 
 export class UsersController {
-  static getAll(req: Request, res: Response) {
-    res.send(usuarios);
+  static async getAll(req: Request, res: Response) {
+    const snapshot = getFirestore().collection("users").get();
+    const users = (await snapshot).docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    });
+    res.send(users);
   }
 
   static getById(req: Request, res: Response) {
