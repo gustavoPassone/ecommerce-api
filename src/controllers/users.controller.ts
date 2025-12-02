19fcad54a1c3a4120f82f85eb7a1568db957986d
gustavoPassone <gmpassone@gmail.com>
2025-12-02 11:00:47 -0300

    feat: atualizando e excluindo dados no Firestore

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -6,7 +6,6 @@ type User = {
   nome: string;
   email: string;
 };
-let usuarios: User[] = [];
 
 export class UsersController {
   static async getAll(req: Request, res: Response) {
@@ -38,20 +37,24 @@ export class UsersController {
   }
 
   static update(req: Request, res: Response) {
-    let userId = Number(req.params.id);
-    let user = req.body;
-    let indexOf = usuarios.findIndex((_user: User) => _user.id === userId);
-    usuarios[indexOf].nome = user.nome;
-    usuarios[indexOf].email = user.email;
+    let userId = req.params.id;
+    let user = req.body as User;
+
+    getFirestore().collection("users").doc(userId).set({
+      nome: user.nome,
+      email: user.email,
+    });
+
     res.send({
       message: "Usuario alterado com sucesso",
     });
   }
 
-  static delete(req: Request, res: Response) {
-    let userId = Number(req.params.id);
-    let indexOf = usuarios.findIndex((user: User) => user.id === userId);
-    usuarios.splice(indexOf, 1);
+  static async delete(req: Request, res: Response) {
+    let userId = req.params.id;
+
+    await getFirestore().collection("users").doc(userId).delete();
+
     res.send({
       message: "Usuario excluido com sucesso",
     });
