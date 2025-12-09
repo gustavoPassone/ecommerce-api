3ca46a97f0c0e314738fce91e47e6363fcd9d4d0
gustavoPassone <gmpassone@gmail.com>
2025-12-09 10:42:40 -0300

    Exceções customizadas

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,78 +1,87 @@
 import { NextFunction, Request, Response } from "express";
 import { getFirestore } from "firebase-admin/firestore";
+import { ValidationError } from "../errors/validation.error";
 
 type User = {
-  id: number;
-  nome: string;
-  email: string;
+    id: number;
+    nome: string;
+    email: string;
 };
 
 export class UsersController {
-  static async getAll(req: Request, res: Response, next: NextFunction) {
-    try {
-      const snapshot = getFirestore().collection("users").get();
-      const users = (await snapshot).docs.map((doc) => {
-        return {
-          id: doc.id,
-          ...doc.data(),
-        };
-      });
-      res.send(users);
-    } catch (error) {
-      next(error);
+    static async getAll(req: Request, res: Response, next: NextFunction) {
+        try {
+            const snapshot = getFirestore().collection("users").get();
+            const users = (await snapshot).docs.map((doc) => {
+                return {
+                    id: doc.id,
+                    ...doc.data(),
+                };
+            });
+            res.send(users);
+        } catch (error) {
+            next(error);
+        }
     }
-  }
 
-  static async getById(req: Request, res: Response, next: NextFunction) {
-    try {
-      let userId = req.params.id;
-      const doc = await getFirestore().collection("users").doc(userId).get();
-      res.send({
-        id: doc.id,
-        ...doc.data(),
-      });
-    } catch (error) {
-      next(error);
+    static async getById(req: Request, res: Response, next: NextFunction) {
+        try {
+            let userId = req.params.id;
+            const doc = await getFirestore()
+                .collection("users")
+                .doc(userId)
+                .get();
+            res.send({
+                id: doc.id,
+                ...doc.data(),
+            });
+        } catch (error) {
+            next(error);
+        }
     }
-  }
 
-  static async save(req: Request, res: Response, next: NextFunction) {
-    try {
-      let user = req.body;
-      const userSalvo = await getFirestore().collection("users").add(user);
-      res.status(201).send({
-        message: `Usuário ${userSalvo.id} criado com sucesso!`,
-      });
-    } catch (error) {
-      next(error);
+    static async save(req: Request, res: Response, next: NextFunction) {
+        try {
+            let user = req.body;
+            if (!user.email || user.email?.length === 0) {
+                throw new ValidationError("Email obrigatorio");
+            }
+            const userSalvo = await getFirestore()
+                .collection("users")
+                .add(user);
+            res.status(201).send({
+                message: `Usuário ${userSalvo.id} criado com sucesso!`,
+            });
+        } catch (error) {
+            next(error);
+        }
     }
-  }
 
-  static update(req: Request, res: Response, next: NextFunction) {
-    try {
-      let userId = req.params.id;
-      let user = req.body as User;
+    static update(req: Request, res: Response, next: NextFunction) {
+        try {
+            let userId = req.params.id;
+            let user = req.body as User;
 
-      getFirestore().collection("users").doc(userId).set({
-        nome: user.nome,
-        email: user.email,
-      });
+            getFirestore().collection("users").doc(userId).set({
+                nome: user.nome,
+                email: user.email,
+            });
 
-      res.send({
-        message: "Usuario alterado com sucesso",
-      });
-    } catch (error) {
-      next(error);
+            res.send({
+                message: "Usuario alterado com sucesso",
+            });
+        } catch (error) {
+            next(error);
+        }
     }
-  }
 
-  static async delete(req: Request, res: Response, next: NextFunction) {
-    try {
-      let userId = req.params.id;
-      await getFirestore().collection("users").doc(userId).delete();
-      res.status(204).end();
-    } catch (error) {
-      next(error);
+    static async delete(req: Request, res: Response, next: NextFunction) {
+        try {
+            let userId = req.params.id;
+            await getFirestore().collection("users").doc(userId).delete();
+            res.status(204).end();
+        } catch (error) {
+            next(error);
+        }
     }
-  }
 }
--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -1,9 +1,17 @@
 import express, { NextFunction, Request, Response } from "express";
+import { ValidationError } from "../errors/validation.error";
+import { InteralServerError } from "../errors/internal-server.error";
 
 export const errorHandler = (app: express.Express) => {
-  app.use((Error: Error, req: Request, res: Response, next: NextFunction) => {
-    res.status(500).send({
-      message: "Erro interno do servidor",
+    app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+        if (error instanceof ValidationError) {
+            error.send(res);
+        } else {
+            new InteralServerError().send(res);
+        }
+
+        res.status(500).send({
+            message: "Erro interno do servidor",
+        });
     });
-  });
 };
