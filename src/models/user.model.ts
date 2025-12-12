966c5dc4f6f916112bf94a690ea825eda4c3e647
gustavoPassone <gmpassone@gmail.com>
2025-12-12 10:35:50 -0300

    Refatorar o user controller para utilizar o UserService nas operações de banco de dados

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,12 +1,12 @@
 import { Joi } from "celebrate";
 
 export type User = {
-    id: number;
+    id: string;
     nome: string;
     email: string;
 };
 
 export const userSchema = Joi.object().keys({
     nome: Joi.string().required(),
-    email: Joi.string().email().required(),
+    email: Joi.string().email().required()
 });
